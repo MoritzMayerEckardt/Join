994c06a1f54243c7844ef57228974cbb78baa730
MarcoBernert <marcobernert111@gmail.com>
2024-03-15 15:12:24 +0100

    changed keyframes in contacts

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,14 @@
 function openDialogAddContacts() {
     let dialog = document.getElementById('dialog-add-contacts')
     dialog.classList.remove('d-none');
-    dialog.style.animation = 'slideInFromRight 0.250s ease-in-out';  
-    dialog.style.right = '0';  
+    dialog.style.animation = 'slideInFromRight 0.250s ease-in-out';
+    dialog.style.right = '0';
 }
 
 function closeDialog() {
     let dialog = document.getElementById('dialog-add-contacts');
-    dialog.style.animation = 'slideOutToRight 0.3s ease-in-out';  
-    dialog.style.right = '-200%';  
+    dialog.style.animation = 'slideOutToRight 0.3s ease-in-out';
+    dialog.style.right = '-200%';
     setTimeout(() => {
         dialog.classList.add('d-none');
     }, 250);
@@ -20,5 +20,22 @@ function doNotClose(event) {
 
 function addContact() {
     console.log('Kontakt wurde erfolgreich hinzugefügt')
-    closeDialog();
+    let dialog = document.getElementById('dialog-add-contacts');
+    dialog.classList.add('d-none');
+    showSlideContainer();
+    clearAddContactForm();
+}
+
+function showSlideContainer() {
+    let slideContainer = document.getElementById('confirmation-field');
+    slideContainer.classList.add('confirmation-field-active');
+    setTimeout(function () {
+        slideContainer.classList.remove('confirmation-field-active');
+    }, 1500); // Die Gesamtdauer der Animation beträgt 2,5 Sekunden
+}
+
+function clearAddContactForm() {
+    document.getElementById('name-input-field-add-contact').value = '';
+    document.getElementById('email-input-field-add-contact').value = '';
+    document.getElementById('phone-input-field-add-contact').value = '';
 }
