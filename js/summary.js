a714a8d9450fe7bd951a7a5ed1e9f34237b915e6
MarcoBernert <marcobernert111@gmail.com>
2024-03-20 11:00:40 +0100

    added function greetBasedOnTime in summary.js

--- js/summary.js
+++ js/summary.js
@@ -1,4 +1,22 @@
 async function initSummary() {
     await includeHTML();
     addBackgroundColor(0);
+    greetBasedOnTime();
 }
+
+
+function greetBasedOnTime() {
+    const now = new Date();
+    const hour = now.getHours();
+    let greeting = "";
+
+    if (hour >= 3 && hour < 12) {
+        greeting = "Good morning,";
+    } else if (hour >= 12 && hour < 17) {
+        greeting = "Good afternoon,";
+    } else {
+        greeting = "Good evening,";
+    }
+    document.getElementById('greet').innerHTML = greeting;
+}
+
