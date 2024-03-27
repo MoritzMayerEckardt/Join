7f8fb317d8d98118d471dbdb6bb77af47e87dffb
MarcoBernert <marcobernert111@gmail.com>
2024-03-27 15:27:16 +0100

    changed sign_up.js

--- js/sign_up.js
+++ js/sign_up.js
@@ -1,8 +1,18 @@
-let emailExists = false;
-
+let emailExists;
+let currentUserdata;
 
 async function initSignUp() {
     await loadUsers();
+}
+
+
+async function register() {
+    await loadUsers();
+    createUsersIfNotCreated();
+    pushDataToUsers();
+    await postData(USERS_PATH, users);
+    emailExists = false;
+    currentUserdata = '';
 }
 
 function createUsersIfNotCreated() {
@@ -12,48 +22,10 @@ function createUsersIfNotCreated() {
 }
 
 
-async function register() {
-    createUsersIfNotCreated();
-    pushDataToUsers();
-    await postData(USERS_PATH, users);
+function pushDataToUsers() {
+    getDataFromInput();
+    users.push(currentUserdata);
 }
-
-
-
-async function pushDataToUsers() {
-    let data = getDataFromInput();
-    users.push(data);
-    return users;
-}
-
-// function getDataFromInput() {
-//     let userName = document.getElementById('inputSingUpName');
-//     let userEmail = document.getElementById('inputSingUpEmail');
-//     let userPassword = document.getElementById('password');
-//     let confirmationPassword = document.getElementById('confirmationPassword');
-//     let emailExists = false;
-
-//     users.forEach(function (user) {
-//         if (user.email == userEmail.value) {
-//             emailExists = true;
-//         }
-//     });
-
-//     if(!emailExists){
-//     if (userPassword.value === confirmationPassword.value) {
-//         if (users && users.length > 0) {
-//             userId = users.length;
-//         } else {
-//             userId = 0;
-//         }
-//         let data = { "name": userName.value, "email": userEmail.value, "password": userPassword.value, "id": userId, "contacts": "", "tasks": "" };
-//         return data;
-//     } else {
-//         alert("The passwords you provided do not match. Please try again.")
-//     }}else {
-//         alert("Your email is already registered.")
-//     }
-// }
 
 
 function getDataFromInput() {
@@ -61,46 +33,52 @@ function getDataFromInput() {
     let userEmail = document.getElementById('inputSingUpEmail').value;
     let userPassword = document.getElementById('password').value;
     let confirmationPassword = document.getElementById('confirmationPassword').value;
-
-
+    
     checkIfEmailExists(userEmail);
 
-
     if (!emailExists) {
-        if (userPassword === confirmationPassword) {
-            let userId;
-            if (users && users.length > 0) {
-                userId = users.length;
-            } else {
-                userId = 0;
-            }
-            let data = { "name": userName, "email": userEmail, "password": userPassword, "id": userId, "contacts": "", "tasks": "" };
-            return data;
-        } else {
-            alert("The passwords you provided do not match. Please try again.");
-        }
+        createJsonForUsers(userName, userEmail, userPassword, confirmationPassword)
     } else {
         alert("Your email is already registered.");
     }
 }
 
-function checkIfEmailExists() {
-
+function checkIfEmailExists(userEmail) {
     if (users && users.length > 0) {
         users.forEach(function (user) {
 
             if (user.email === userEmail) {
                 emailExists = true;
-            } else {
-                emailExists = false;
-            }
+            } 
+            // else {
+            //     emailExists = false;
+            // }
         });
     } else {
         emailExists = false;
     }
 }
 
-
+function createJsonForUsers(userName, userEmail, userPassword, confirmationPassword){
+    if (userPassword === confirmationPassword) {
+        let userId;
+        if (users && users.length > 0) {
+            userId = users.length;
+        } else {
+            userId = 0;
+        }
+        currentUserdata = {
+            "name": userName,
+            "email": userEmail,
+            "password": userPassword,
+            "id": userId,
+            "contacts": "",
+            "tasks": ""
+        };        
+    } else {
+        alert("The passwords you provided do not match. Please try again.");
+    }
+}
 
 
 async function postData(path = "/users") {
@@ -148,3 +126,47 @@ async function deleteUser() {
     users.splice(1, 1);
     await postData(USERS_PATH, users);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getDataFromInput() {
+//     let userName = document.getElementById('inputSingUpName');
+//     let userEmail = document.getElementById('inputSingUpEmail');
+//     let userPassword = document.getElementById('password');
+//     let confirmationPassword = document.getElementById('confirmationPassword');
+//     let emailExists = false;
+
+//     users.forEach(function (user) {
+//         if (user.email == userEmail.value) {
+//             emailExists = true;
+//         }
+//     });
+
+//     if(!emailExists){
+//     if (userPassword.value === confirmationPassword.value) {
+//         if (users && users.length > 0) {
+//             userId = users.length;
+//         } else {
+//             userId = 0;
+//         }
+//         let data = { "name": userName.value, "email": userEmail.value, "password": userPassword.value, "id": userId, "contacts": "", "tasks": "" };
+//         return data;
+//     } else {
+//         alert("The passwords you provided do not match. Please try again.")
+//     }}else {
+//         alert("Your email is already registered.")
+//     }
+// }
