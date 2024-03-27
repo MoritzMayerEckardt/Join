ed7a9a62a6783a71d33d18d2b31a374883350b9c
MarcoBernert <marcobernert111@gmail.com>
2024-03-27 15:36:32 +0100

    clean code in signup.js

--- js/sign_up.js
+++ js/sign_up.js
@@ -5,14 +5,12 @@ async function initSignUp() {
     await loadUsers();
 }
 
-
 async function register() {
     await loadUsers();
     createUsersIfNotCreated();
     pushDataToUsers();
     await postData(USERS_PATH, users);
-    emailExists = false;
-    currentUserdata = '';
+    resetVariables();
 }
 
 function createUsersIfNotCreated() {
@@ -21,19 +19,17 @@ function createUsersIfNotCreated() {
     }
 }
 
-
 function pushDataToUsers() {
     getDataFromInput();
     users.push(currentUserdata);
 }
-
 
 function getDataFromInput() {
     let userName = document.getElementById('inputSingUpName').value;
     let userEmail = document.getElementById('inputSingUpEmail').value;
     let userPassword = document.getElementById('password').value;
     let confirmationPassword = document.getElementById('confirmationPassword').value;
-    
+
     checkIfEmailExists(userEmail);
 
     if (!emailExists) {
@@ -49,17 +45,14 @@ function checkIfEmailExists(userEmail) {
 
             if (user.email === userEmail) {
                 emailExists = true;
-            } 
-            // else {
-            //     emailExists = false;
-            // }
+            }
         });
     } else {
         emailExists = false;
     }
 }
 
-function createJsonForUsers(userName, userEmail, userPassword, confirmationPassword){
+function createJsonForUsers(userName, userEmail, userPassword, confirmationPassword) {
     if (userPassword === confirmationPassword) {
         let userId;
         if (users && users.length > 0) {
@@ -74,12 +67,11 @@ function createJsonForUsers(userName, userEmail, userPassword, confirmationPassw
             "id": userId,
             "contacts": "",
             "tasks": ""
-        };        
+        };
     } else {
         alert("The passwords you provided do not match. Please try again.");
     }
 }
-
 
 async function postData(path = "/users") {
     try {
@@ -99,7 +91,7 @@ async function postData(path = "/users") {
         console.log("Daten erfolgreich gesendet:", data);
         if (response.ok) {
             resetSignUpForm();
-            // window.location.href = `login.html?msg=successfully_registered`;
+            window.location.href = `login.html?msg=successfully_registered`;
         }
 
     } catch (error) {
@@ -107,6 +99,10 @@ async function postData(path = "/users") {
     }
 }
 
+function resetVariables(){
+    emailExists = false;
+    currentUserdata = '';
+}
 
 function resetSignUpForm() {
     document.getElementById('inputSingUpName').value = '';
@@ -114,8 +110,6 @@ function resetSignUpForm() {
     document.getElementById('password').value = '';
     confirmationPassword = document.getElementById('confirmationPassword').value = '';
 }
-
-
 
 async function deleteAllUsers() {
     users = [];
@@ -126,47 +120,3 @@ async function deleteUser() {
     users.splice(1, 1);
     await postData(USERS_PATH, users);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function getDataFromInput() {
-//     let userName = document.getElementById('inputSingUpName');
-//     let userEmail = document.getElementById('inputSingUpEmail');
-//     let userPassword = document.getElementById('password');
-//     let confirmationPassword = document.getElementById('confirmationPassword');
-//     let emailExists = false;
-
-//     users.forEach(function (user) {
-//         if (user.email == userEmail.value) {
-//             emailExists = true;
-//         }
-//     });
-
-//     if(!emailExists){
-//     if (userPassword.value === confirmationPassword.value) {
-//         if (users && users.length > 0) {
-//             userId = users.length;
-//         } else {
-//             userId = 0;
-//         }
-//         let data = { "name": userName.value, "email": userEmail.value, "password": userPassword.value, "id": userId, "contacts": "", "tasks": "" };
-//         return data;
-//     } else {
-//         alert("The passwords you provided do not match. Please try again.")
-//     }}else {
-//         alert("Your email is already registered.")
-//     }
-// }
