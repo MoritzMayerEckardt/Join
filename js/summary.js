286fd2196f726f7dfcaf3e0fa9f7a41528ef4a52
MarcoBernert <marcobernert111@gmail.com>
2024-03-29 15:58:58 +0100

    changed name function in summary

--- js/summary.js
+++ js/summary.js
@@ -1,22 +1,47 @@
+let greeting;
+
 async function initSummary() {
     await includeHTML();
+    await loadUsers();
+    await loadCurrentUserIndex();
     addBackgroundColor(0);
     greetBasedOnTime();
+    renderData();
 }
+
+
 
 
 function greetBasedOnTime() {
     const now = new Date();
     const hour = now.getHours();
-    let greeting = "";
+    greeting = "";
 
     if (hour >= 3 && hour < 12) {
-        greeting = "Good morning,";
+        greeting = "Good morning";
     } else if (hour >= 12 && hour < 17) {
-        greeting = "Good afternoon,";
+        greeting = "Good afternoon";
     } else {
-        greeting = "Good evening,";
+        greeting = "Good evening";
     }
     document.getElementById('greet').innerHTML = greeting;
 }
 
+
+function renderData(){
+    renderCurrentUserName();
+    console.log('der aktuelle Nutzer ist')
+}
+
+function renderCurrentUserName() {
+
+    let greetingName = document.getElementById('greeting-name');
+
+    if(currentUserIndex == 'guestLogin') {
+        greetingName.innerHTML = '';
+    } else {
+        let name = users[currentUserIndex]['name']
+        greetingName.innerHTML = name;
+        document.getElementById('greet').innerHTML = greeting + ',';
+    }
+}
